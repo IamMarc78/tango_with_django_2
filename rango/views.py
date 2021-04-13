1dc44dd8c4a83eb46c136e504b24f117294a92e2
Marc <marc.pieniazek@protonmail.com>
2021-04-13 15:53:06 +0100

    Chapter 5 complete. Tests passed

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -10,6 +10,7 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
